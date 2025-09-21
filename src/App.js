8cda6e0b3dc12496c4bef78fa1e11cf5c9d6cc7a
Bhall2023 <147952158+Bhall2023@users.noreply.github.com>
2025-09-21 11:02:15 -0500

    added search engine feature

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from "./logo.svg";
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <h1>Dictionary App</h1>
         <img src={logo} alt="logo" className="App-logo img-fluid" />
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        <small>Coded by Bonnie Hall</small>
+      </footer>
+      </div>
     </div>
   );
 }
