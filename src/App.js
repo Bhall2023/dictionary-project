5b514db6e50798031cf2b1e0eb77a78c12dda4f6
Bhall2023 <147952158+Bhall2023@users.noreply.github.com>
2025-09-21 09:59:30 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Dictionary App</h1>
+        <img src={logo} alt="logo" className="App-logo img-fluid" />
+      </header>
+    </div>
+  );
+}
+
+
