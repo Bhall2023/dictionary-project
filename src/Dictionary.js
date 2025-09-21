c79079376547da51d0e29293a520f196e342796a
Bhall2023 <147952158+Bhall2023@users.noreply.github.com>
2025-09-21 16:34:26 -0500

    added photo functionality to the application and styling

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,21 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
     let [keyword, setKeyword] = useState(props.defaultKeyword);
     let [results, setResults] = useState(null); 
     let [loaded, setLoaded] = useState(false);
+    let [photos, setPhotos] = useState(null);   
 
-    function handleResponse(response) {
-        console.log(response.data[0]);
+    function handleDictionaryResponse(response) {
         setResults(response.data[0]);
         setLoaded(true);
+    }
+    function handleSheCodesResponse(response) {
+        setPhotos(response.data.photos);    
     }
     
     function search() {
@@ -19,8 +23,15 @@ export default function Dictionary(props) {
      if (!q) { setLoaded(true); return; }    
      //documentation: https://dictionaryapi.dev/
      let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-     axios.get(apiUrl).then(handleResponse);
-    }  
+     axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let sheCodesApiKey = "7689111d9btfeef8e4e3cc0cba8be0o7";
+    let sheCodesApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${sheCodesApiKey}`;
+    axios.get(sheCodesApiUrl).then(handleSheCodesResponse).catch(() => setPhotos([]));
+    }
+
+
+    
 
     function handleSubmit(event) {
         event.preventDefault();
@@ -43,6 +54,7 @@ export default function Dictionary(props) {
         </form>
         
         {results && <Results results={results} />}
+        {photos && photos.length > 0 && <Photos photos={photos} />}
       </section>
     );
 }   
